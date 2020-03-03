bfc322932644763a831d3ca4382d6a1be69943aa
Fabio Junio d Silva Gonçalves <fjunio.webdeveloper@outlook.com>
2020-03-03 17:46:02 -0300

    Criando a classe posição da camada tabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tabuleiro;
 
 namespace Xadrez
 {
@@ -6,7 +7,9 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao p = new Posicao(3, 4);
+            Console.WriteLine("Posição: " + p);
+            Console.ReadKey();
         }
     }
 }
---
+++ Xadrez/Tabuleiro/Posicao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tabuleiro
+{
+    public class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            this.Linha = linha;
+            this.Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return $"{Linha}, {Coluna}";
+        }
+    }
+}
